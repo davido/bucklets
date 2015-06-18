cfad992243b3b68e8cca957cca5caf8e7ee8d7a3
Hugo Arès <hugo.ares@ericsson.com>
2015-06-18 10:40:46 -0400

    Fix generation of eclipse project
    When generating eclipse project using bucklets/tools/eclipse.py, the
    source folders were not generated.
    
    Change-Id: Ib50d37c12fb3cb750d124e9aef8ba8528ede6873

--- tools/eclipse.py
+++ tools/eclipse.py
@@ -87,7 +87,7 @@ def gen_classpath():
   src = set()
   lib = set()
 
-  java_library = re.compile(r'[^/]+/gen/(.*)/lib__[^/]+__output/[^/]+[.]jar$')
+  java_library = re.compile(r'[^/]+/gen(.*)/lib__[^/]+__output/[^/]+[.]jar$')
   for p in query_classpath(MAIN):
     m = java_library.match(p)
     if m:
